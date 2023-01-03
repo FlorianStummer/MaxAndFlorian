60b720ce9225b17cd0a47890b14ac4a5491b8b60
Max <max.prokop@outlook.de>
2023-01-03 01:34:14 +0100

    dataset with small npzs

--- Unet/CustomDatasets.py
+++ Unet/CustomDatasets.py
@@ -35,3 +35,37 @@ class MagnetDataset(Dataset):
             #     invert current
 
         return input, target
+
+
+class SingleMagnetDataset(Dataset):
+    def __init__(self, path_to_root, partition=(0,1), transforms=None, maximum_elements=10000):
+        self.path_to_root = path_to_root
+        self.transforms = transforms
+        self.partition = partition
+        self.maximum_elements = maximum_elements
+        self.lowest_idx = int(self.partition[0]*(self.maximum_elements))
+        self.highest_idx = int(self.partition[1]*(self.maximum_elements) - 1)
+        self.length = min(self.maximum_elements,self.highest_idx - self.lowest_idx)
+        self.train = self.partition[0] == 0 # this assumes the dataset partition starts with the train segment
+        # self.dim_list = np.genfromtxt(os.path.join(self.path_to_root,"random_magnet_list.c
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        actual_idx = self.lowest_idx + idx
+
+        sample = np.load(os.path.join(self.path_to_root,str(actual_idx)+".npz"))
+        
+        inp = sample["input"]
+        tar = sample["target"]
+        
+        # data augmentation
+        # if(model in trainmode):
+            # Mirrored in X
+            # if(np.random.normal(0,1,1) <= 0.5):
+            #     mirror in x
+            # Negative current
+            # if(np.random.normal(0,1,1) <= 0.5):
+            #     invert current
+
+        return inp, tar
