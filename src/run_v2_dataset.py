cd308fb54442f0e46289924ff1b7f9ea8dc16a43
flstumme <florian.wolfgang.stummer@cern.ch>
2024-09-10 23:58:06 +0200

    cleanup

--- src/run_v2_dataset.py
+++ src/run_v2_dataset.py
@@ -53,11 +53,15 @@ def main():
     # set device
     if torch.cuda.is_available():
         device = torch.device("cuda")
+        print(torch.cuda.get_device_name(0))
+        print("Memory Allocated:", torch.cuda.memory_allocated(0))
+        print("Memory Cached:", torch.cuda.memory_reserved(0))
     elif torch.backends.mps.is_available():
         device = torch.device("mps")
     else:
         device = torch.device("cpu")
     print("Using", device)
+    
 
     # load model
     model = UNet(in_channels=18, out_channels=2, depth=depth, wf=wf, padding=padding, up_mode=up_mode, batch_norm=batch_norm, drop_out=drop_out)
