f51cf3487c805132f6d1dea91288f77dbe697e19
max_gpu_server <max.on@server.to>
2024-11-14 10:10:51 +0100

    Try

--- src/final_sprint/data/datasets.py
+++ src/final_sprint/data/datasets.py
@@ -33,7 +33,7 @@ class _MyDataset(_Dataset):
         #                         'coilVolumeTotal', 'coolingRequirementMax', 'length', 'material_coil',
         #                         'material_yoke', 'yokeAreaTotal', 'yokeWeightTotal', 'yokeVolumeTotal']
         self.target_columns = target_columns
-        self.input_columns = [col for col in self.dataframe.columns if col not in self.target_columns and col != 'name']
+        self.input_columns = [col for col in self.dataframe.columns if col not in self.target_columns and col != 'name' and "subset" not in col]
         if input_columns is not None:
             self.input_columns = input_columns
         if target_columns is not None:
@@ -53,19 +53,6 @@ class _MyDataset(_Dataset):
         target = self.targets.iloc[idx].values.astype(float)
         return inputs, target
 
-
-
-# _df_a = _pd.read_csv(_pkg_resources.resource_filename(__name__, './data/hdipole_gfr_dataset_00k-30k.csv'))
-# _df_b = _pd.read_csv(_pkg_resources.resource_filename(__name__, './data/hdipole_gfr_dataset_30k-60k.csv'))
-_df_a = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_straight.csv"))
-_df_b = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_random.csv"))
-_df_c = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_random_small.csv"))
-_df_d = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_random_large.csv"))
-_df = _pd.concat([_df_a, _df_b, _df_c, _df_d], ignore_index=True)
-# exchange all NaN values of Cooling Requirement Max with 'None' (string was misinterpreted as NaN)
-_df["coolingRequirementMax"] = _df["coolingRequirementMax"].fillna("None")
-_df = _df.drop(columns=["Unnamed: 0"])
-# _target_columns = ['B0', 'gfr_x_1e-4', 'gfr_y_1e-4']
 _target_columns_dipole_h = ['B0', 'grad0', 'gfr_x_1e-7', 'gfr_x_2e-7', 'gfr_x_5e-7', 'gfr_x_1e-6',
     'gfr_x_2e-6', 'gfr_x_5e-6', 'gfr_x_1e-5', 'gfr_x_2e-5', 'gfr_x_5e-5',
     'gfr_x_1e-4', 'gfr_x_2e-4', 'gfr_x_5e-4', 'gfr_x_1e-3', 'gfr_x_2e-3',
@@ -74,8 +61,36 @@ _target_columns_dipole_h = ['B0', 'grad0', 'gfr_x_1e-7', 'gfr_x_2e-7', 'gfr_x_5e
     'gfr_y_5e-5', 'gfr_y_1e-4', 'gfr_y_2e-4', 'gfr_y_5e-4', 'gfr_y_1e-3',
     'gfr_y_2e-3', 'gfr_y_5e-3', 'gfr_y_1e-2']
 
-Dipole_H_unshuffeled = _MyDataset(_df,target_columns=_target_columns_dipole_h)
 
-_df = _df.sample(frac=1, random_state=42)
-Dipole_H_train = _MyDataset(_df.iloc[:int(0.8*len(_df))],target_columns=_target_columns_dipole_h)
-Dipole_H_val = _MyDataset(_df.iloc[int(0.8*len(_df)):],target_columns=_target_columns_dipole_h)
+# _df_a = _pd.read_csv(_pkg_resources.resource_filename(__name__, './data/hdipole_gfr_dataset_00k-30k.csv'))
+# _df_b = _pd.read_csv(_pkg_resources.resource_filename(__name__, './data/hdipole_gfr_dataset_30k-60k.csv'))
+_df_a = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_straight.csv"))
+_df_b = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_random.csv"))
+_df_c = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_random_small.csv"))
+_df_d = _pd.read_csv(_pkg_resources.resource_filename(__name__,"md_dipole_hshaped_v2_random_large.csv"))
+_df_b = _df_b.sample(frac=1, random_state=42)
+_df_c = _df_c.sample(frac=1, random_state=43)
+_df_d = _df_d.sample(frac=1, random_state=44)
+
+
+frac=0.7
+_df_b_A = _df_b.iloc[:int(frac*(len(_df_b)))]
+_df_b_B = _df_b.iloc[int(frac*(len(_df_b))):]
+_df_c_A = _df_c.iloc[:int(frac*(len(_df_c)))]
+_df_c_B = _df_c.iloc[int(frac*(len(_df_c))):]
+_df_d_A = _df_d.iloc[:int(frac*(len(_df_d)))]
+_df_d_B = _df_d.iloc[int(frac*(len(_df_d))):]
+
+_df_train = _pd.concat([_df_a, _df_b_A, _df_c_A, _df_d_A], ignore_index=True)
+# exchange all NaN values of Cooling Requirement Max with 'None' (string was misinterpreted as NaN)
+_df_train["coolingRequirementMax"] = _df_train["coolingRequirementMax"].fillna("None")
+_df_train = _df_train.drop(columns=["Unnamed: 0"])
+Dipole_H_train = _MyDataset(_df_train,target_columns=_target_columns_dipole_h)
+
+_df_val = _pd.concat([_df_b_B, _df_c_B, _df_d_B], ignore_index=True)
+# exchange all NaN values of Cooling Requirement Max with 'None' (string was misinterpreted as NaN)
+_df_val["coolingRequirementMax"] = _df_val["coolingRequirementMax"].fillna("None")
+_df_val = _df_val.drop(columns=["Unnamed: 0"])
+
+Dipole_H_val = _MyDataset(_df_val,target_columns=_target_columns_dipole_h)
+# _target_columns = ['B0', 'gfr_x_1e-4', 'gfr_y_1e-4']
